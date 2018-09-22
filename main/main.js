904e20a515423216191dc4ae7ad57f86e52f94bf
shadowswind <731246251@qq.com>
2018-09-22 18:13:06 +0800

    second commit

--- main/main.js
+++ main/main.js
@@ -1,18 +1,31 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.count()}
+o) 平均值 = ${sequence.average()}`
+);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.input = input;
   }
 
   minimum() {
-    // Write your code here
+    return Math.min.apply(null,this.input);
   }
-
+  maximum(){
+    return Math.max.apply(null,this.input);
+  }
+  count(){
+    return this.input.length;
+  }
+  average(){
+    var sum = this.input.reduce((a,b) =>{
+      return a + b;
+    })
+    return (sum / this.input.length).toFixed(2);
+  }
   // Write your code here
 }
